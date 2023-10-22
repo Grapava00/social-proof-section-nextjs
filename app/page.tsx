d2de0599fdc477b5aca539f60269a189c4d504f2
Giorgi Rapava <grapava00@gmail.com>
2023-10-22 22:13:55 +0400

    project done

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,187 @@
-import Image from 'next/image'
+/**
+ * responsive font-size
+ *
+ */
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className='main-content'>
+      <div className='container'>
+        <div className='flex-container'>
+          <div className='content'>
+            <h1 className='content-title'>
+              10,000+ of our users love our products.
+            </h1>
+            <p className='content-description'>
+              We only provide great products combined with excellent customer
+              service. See what our satisfied customers are saying about our
+              services.
+            </p>
+          </div>
+          <div>
+            <div className='rating-section raiting-section-one'>
+              <div className='stars'>
+                <img
+                  className='star-icon'
+                  src='/icon-star.svg'
+                  alt='Star Icon'
+                />
+                <img
+                  className='star-icon'
+                  src='/icon-star.svg'
+                  alt='Star Icon'
+                />
+                <img
+                  className='star-icon'
+                  src='/icon-star.svg'
+                  alt='Star Icon'
+                />
+                <img
+                  className='star-icon'
+                  src='/icon-star.svg'
+                  alt='Star Icon'
+                />
+                <img
+                  className='star-icon'
+                  src='/icon-star.svg'
+                  alt='Star Icon'
+                />
+              </div>
+              <p className='rating-text'>Rated 5 Stars in Reviews</p>
+            </div>
+            <div className='rating-section raiting-section-two'>
+              <div className='stars'>
+                <img
+                  className='star-icon'
+                  src='/icon-star.svg'
+                  alt='Star Icon'
+                />
+                <img
+                  className='star-icon'
+                  src='/icon-star.svg'
+                  alt='Star Icon'
+                />
+                <img
+                  className='star-icon'
+                  src='/icon-star.svg'
+                  alt='Star Icon'
+                />
+                <img
+                  className='star-icon'
+                  src='/icon-star.svg'
+                  alt='Star Icon'
+                />
+                <img
+                  className='star-icon'
+                  src='/icon-star.svg'
+                  alt='Star Icon'
+                />
+              </div>
+              <p className='rating-text'>Rated 5 Stars in Report Guru</p>
+            </div>
+            <div className='rating-section'>
+              <div className='stars'>
+                <img
+                  className='star-icon'
+                  src='/icon-star.svg'
+                  alt='Star Icon'
+                />
+                <img
+                  className='star-icon'
+                  src='/icon-star.svg'
+                  alt='Star Icon'
+                />
+                <img
+                  className='star-icon'
+                  src='/icon-star.svg'
+                  alt='Star Icon'
+                />
+                <img
+                  className='star-icon'
+                  src='/icon-star.svg'
+                  alt='Star Icon'
+                />
+                <img
+                  className='star-icon'
+                  src='/icon-star.svg'
+                  alt='Star Icon'
+                />
+              </div>
+              <p className='rating-text'>Rated 5 Stars in BestTech</p>
+            </div>
+          </div>
+        </div>
+
+        <div className='card-container'>
+          <article className='card'>
+            <div className='card-profile'>
+              <div className='profile-image'>
+                <img
+                  className='profile-image'
+                  src='/image-colton.jpg'
+                  alt='Profile'
+                />
+              </div>
+              <div className='profile-info'>
+                <p className='profile-name'>Colton Smith</p>
+                <p className='profile-username'>Verified Buyer</p>
+              </div>
+            </div>
+            <div className='card-content'>
+              <p className='content-text'>
+                "We needed the same printed design as the one we had ordered a
+                week prior. Not only did they find the original order, but we
+                also received it in time. Excellent!"
+              </p>
+            </div>
+          </article>
+          <article className='card'>
+            <div className='card-profile'>
+              <div className='profile-image'>
+                <img
+                  className='profile-image'
+                  src='/image-irene.jpg'
+                  alt='Profile'
+                />
+              </div>
+              <div className='profile-info'>
+                <p className='profile-name'>Irene Roberts</p>
+                <p className='profile-username'>Verified Buyer</p>
+              </div>
+            </div>
+            <div className='card-content'>
+              <p className='content-text'>
+                "Customer service is always excellent and very quick turn
+                around. Completely delighted with the simplicity of the purchase
+                and the speed of delivery."
+              </p>
+            </div>
+          </article>
+          <article className='card'>
+            <div className='card-profile'>
+              <div className='profile-image'>
+                <img
+                  className='profile-image'
+                  src='/image-anne.jpg'
+                  alt='Profile'
+                />
+              </div>
+              <div className='profile-info'>
+                <p className='profile-name'> Anne Wallace </p>
+                <p className='profile-username'>Verified Buyer</p>
+              </div>
+            </div>
+            <div className='card-content'>
+              <p className='content-text'>
+                "Put an order with this company and can only praise them for the
+                very high standard. Will definitely use them again and recommend
+                them to everyone!"
+              </p>
+            </div>
+          </article>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,35 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        "primary-magenta-900": "hsl(300, 43%, 22%)",
+        "primary-pink": "hsl(333, 80%, 67%)",
+        "secondary-magenta-700": "hsl(303, 10%, 53%)",
+        "secondary-magenta-500": "hsl(300, 24%, 96%)",
+      },
+      fontFamily: {
+        league: ["League Spartan", "sans-serif"],
+        
+      },
+      fontWeight: {
+        leagueRegular: "400",
+        leagueMedium: "500",
+        leagueBold: "700",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": `conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))`,
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+
+export default config;
